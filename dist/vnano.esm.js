fe2056247152b4fd21e184bdf7074d1b4b5f0433
N0zzy <ffnn83@yandex.ru>
2026-08-27 23:58:04 +0500

    dist

---
+++ dist/vnano.esm.js
@@ -0,0 +1,13 @@
+import './vnano.js';
+
+const $v = typeof globalThis !== 'undefined' ? globalThis.$v : window.$v;
+
+export const {
+    createApp, signal, computed, effect, batch, h, escapeHtml,
+    patch, createContext, createPortal, forwardRef, createDOMNode, renderToString, hydrateNode, lazy, createRouter,
+    If, Match, Case, Portal, Dynamic, Show, Fragment, Link, RouterView, html,
+    Component, App, Suspense, KeepAlive, Delegator, LazyWrapper,
+    ajaxSetup, ajax, get, post, getJSON, applyHMR
+} = $v;
+
+export default $v;
